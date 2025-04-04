ed3b6155f20a3275ab29eeead636899f49204d41
Oluwatobi <oluwatobirichard001@example.com>
2025-04-04 06:48:31 +0100

    secondt one

--- ojo-oluwatobi-portfolio/src/components/LoadingScreen.jsx
+++ ojo-oluwatobi-portfolio/src/components/LoadingScreen.jsx
@@ -1,39 +1,38 @@
- import { useEffect, useState } from 'react'
- import React from 'react'
- 
-export const LoadingScreen = (onComplete) => {
-    const [text, setText] = useState("");
-    const fullText = "<Hello, World/>" 
+import { useEffect, useState } from 'react';
+import React from 'react';
 
-    useEffect (() => {
-        let index = 0;
-        const interval = setInterval(() => {
-            setText(fullText.substring(0, index));
-            index++;
+export const LoadingScreen = ({ onComplete }) => {
+  const [text, setText] = useState("");
+  const fullText = "<WELCOME TO OLUWATOBI OJO PORTFOLIO/>";
 
-            if (index > fullText.length) {
-                clearInterval(interval)
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, index));
+      index++;
 
-                setTimeout(() =>{
-                     onComplete();
-                }, 1000);
-            }
-        }, 100);
+      if (index > fullText.length) {
+        clearInterval(interval);
 
-        return () => clearInterval(interval);
-    }, [onComplete]);
+        setTimeout(() => {
+          onComplete();
+        }, 1000);
+      }
+    }, 100);
 
-   return (
-     <div className='fixed insert-0 z-50 bg-black text-grey-100 flex flex-col items-center justify-center'>
-        <div className='ab-4 text-4xl font-mono font-bond'>
-             {text} <span className='animate-blink ml-1'></span>
+    return () => clearInterval(interval);
+  }, [onComplete]);
+
+  return (
+    <div className='fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center'>
+      <div className='mb-4 text-4xl font-mono font-bold'>
+        {text} <span className='animate-blink ml-1'></span>
+      </div>
+      <div className='w-[200px] h-[2px] relative rounded overflow-hidden'>
+        <div className='w-[40%] h-full bg-blue-500 shadow[0_0_15px_#3b82f6] animate-loading-bar'>
+          {""}
         </div>
-        <div className='w-[200px] h-[2px]round-relative overflow-hidden'>
-            <div className='w-[40%] h-full bg-blue-500 shadow[0_0_15px_#3b82f6] animate-loading-bar '>
-                {""}
-            </div>
-        </div>
-     </div>
-   )
- };
- 
+      </div>
+    </div>
+  );
+};
